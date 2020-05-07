5255c12ebf3923f0cb3ae8df4f087d96826bf95a
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-05-07 15:00:04 +0300

    Добавлена возможность заходить в комнату, и отправлять сообщения

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,57 @@
-import React from 'react'
-import {Typography} from '@material-ui/core'
-const Chat = () => {
-  return <Typography variant="h3">Chat</Typography>
+import React, {useState, useEffect} from 'react'
+import {Box, TextField} from '@material-ui/core'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+
+let socket
+
+const Chat = ({location}) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const ENDPOINT = 'localhost'
+
+  useEffect(() => {
+    const {name, room} = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+
+    setRoom(room)
+    setName(name)
+
+    socket.emit('join', {name, room}, (error) => {
+      if (error) {
+        alert(error)
+      }
+    })
+  }, [ENDPOINT, location.search])
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((messages) => [...messages, message])
+    })
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
+  return (
+    <Box>
+      <TextField
+        onChange={(e) => setMessage(e.target.value)}
+        id="outlined-basic"
+        label="Outlined"
+        value={message}
+        variant="outlined"
+        onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
+      />
+    </Box>
+  )
 }
 
 export default Chat
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,76 @@
-import React from 'react'
-import {Typography} from '@material-ui/core'
+import React, {useState} from 'react'
+import {
+  Typography,
+  Paper,
+  TextField,
+  Box,
+  Button,
+  Divider,
+} from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
 const Join = () => {
-  return <Typography variant="h3">Join</Typography>
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{
+        height: '100vh',
+      }}
+    >
+      <Paper elevation={2} style={{padding: '2rem', width: '450px'}}>
+        <Box textAlign="center" mb={2}>
+          <Typography variant="h3">Join</Typography>
+          <Divider />
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box width={1} mb={2}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Box>
+          <Box width={1} mb={2}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Room"
+              variant="outlined"
+              onChange={(e) => setRoom(e.target.value)}
+            />
+          </Box>
+          <Box width={1}>
+            <Link
+              onClick={(e) => (!name || !room) && e.preventDefault()}
+              to={`/chat?name=${name}&room=${room}`}
+            >
+              <Button
+                style={{padding: '10px 0'}}
+                fullWidth
+                variant={'outlined'}
+                color={'primary'}
+              >
+                Sign in
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
+  )
 }
 
 export default Join
